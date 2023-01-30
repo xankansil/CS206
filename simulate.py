db95377a95baba4d3efb1f130237ed152c14a54f
xankansil <xantia.kansil@uvm.edu>
2023-01-29 19:12:12 -0500

    creating towers

--- simulate.py
+++ simulate.py
@@ -1,8 +1,18 @@
 import pybullet as p
 import time
+import pybullet_data
+
+
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+p.setGravity(0,0,-9.8)
+planeId = p.loadURDF("plane.urdf")
+p.loadSDF("box.sdf")
+
 for x in range(1000):
 	p.stepSimulation()
 	time.sleep(1/60)
 	print(x)
+	
 p.disconnect()
