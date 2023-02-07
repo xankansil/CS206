d6ef29a0e2ccd3b17dfdc0536329f090f6e4721e
xankansil <xantia.kansil@uvm.edu>
2023-02-06 20:28:19 -0500

    finished front and backleg sens

--- analyze.py
+++ analyze.py
@@ -1,1 +1,11 @@
-import numpy
+import numpy as numpy
+import matplotlib.pyplot as matplotlib
+
+backLegSensorValues = numpy.load("data/backLegSensorValues.npy")
+frontLegSensorValues = numpy.load("data/frontLegSensorValues.npy")
+
+matplotlib.plot(backLegSensorValues, label = "Link0", linewidth = 4)
+matplotlib.plot(frontLegSensorValues, label = "Link2")
+
+matplotlib.legend()
+matplotlib.show()
