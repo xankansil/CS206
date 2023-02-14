7a0f01bc2d0cc42d94628fc884c339865819df72
xankansil <xantia.kansil@uvm.edu>
2023-02-13 21:43:43 -0500

    done motors

--- analyze.py
+++ analyze.py
@@ -1,11 +1,17 @@
 import numpy as numpy
 import matplotlib.pyplot as matplotlib
 
-backLegSensorValues = numpy.load("data/backLegSensorValues.npy")
-frontLegSensorValues = numpy.load("data/frontLegSensorValues.npy")
+#matplotlib.plot(backLegSensorValues, label = "Link0", linewidth = 4)
+#matplotlib.plot(frontLegSensorValues, label = "Link2")
 
-matplotlib.plot(backLegSensorValues, label = "Link0", linewidth = 4)
-matplotlib.plot(frontLegSensorValues, label = "Link2")
+
+backLegSensorValues = numpy.load('data/backLegSensorValues.npy')
+frontLegSensorValues = numpy.load('data/frontLegSensorValues.npy')
+backLegJointValues = numpy.load("data/backLegJointValues.npy")
+frontLegJointValues = numpy.load("data/frontLegJointValues.npy")
+
+matplotlib.plot(backLegJointValues, linewidth=2, label='BackLeg')
+matplotlib.plot(frontLegJointValues, linewidth=2, label='FrontLeg')
 
 matplotlib.legend()
 matplotlib.show()
