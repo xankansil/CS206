0482e034d3700428dabc68e32123655b3d308444
xankansil <xantia.kansil@uvm.edu>
2023-02-20 23:30:00 -0500

    done

---
+++ constants.py
@@ -0,0 +1,21 @@
+import math
+import numpy
+
+pi = math.pi
+
+
+amplitudeBackLeg = pi/4
+frequencyBackLeg = 10
+phaseOffsetBackLeg = 0
+
+amplitudeFrontLeg = pi/4
+frequencyFrontLeg = 15
+phaseOffsetFrontLeg = pi/4
+
+program_run_time = 1000
+
+length = 1000
+
+gravity = -9.8
+
+sleep_time = 1/240
