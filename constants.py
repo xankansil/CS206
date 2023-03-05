2091f246440b8fc74b599628cb40239f515355ff
xankansil <xantia.kansil@uvm.edu>
2023-03-05 14:35:02 -0500

    done

--- constants.py
+++ constants.py
@@ -4,13 +4,29 @@ import numpy
 pi = math.pi
 
 
+##amplitudeBackLeg = pi/4
+##frequencyBackLeg = 10
+##phaseOffsetBackLeg = 0
+##
+##amplitudeFrontLeg = pi/4
+##frequencyFrontLeg = 15
+##phaseOffsetFrontLeg = pi/4
+##
+##amplitudeBackLeg = pi/2
+##frequencyBackLeg = 5
+##phaseOffsetBackLeg = pi/2
+##
+##amplitudeFrontLeg = pi/2
+##frequencyFrontLeg = 10
+##phaseOffsetFrontLeg = 0
+
 amplitudeBackLeg = pi/4
 frequencyBackLeg = 10
 phaseOffsetBackLeg = 0
 
 amplitudeFrontLeg = pi/4
-frequencyFrontLeg = 15
-phaseOffsetFrontLeg = pi/4
+frequencyFrontLeg = 10
+phaseOffsetFrontLeg = 0
 
 program_run_time = 1000
 
@@ -19,3 +35,5 @@ length = 1000
 gravity = -9.8
 
 sleep_time = 1/240
+
+maxForce = 100
