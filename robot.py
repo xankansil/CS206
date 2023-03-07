d1f5a7186d45767146ffb88eac6389084e55f1d8
xankansil <xantia.kansil@uvm.edu>
2023-03-06 22:45:56 -0500

    done

--- robot.py
+++ robot.py
@@ -8,7 +8,8 @@ import constants as c
 from pyrosim.neuralNetwork import NEURAL_NETWORK
 
 class ROBOT:
-    def __init__(self):
+    def __init__(self, s=2, m=2):
+        self.motors = {}
         self.robotId = p.loadURDF("body.urdf")
         pyrosim.Prepare_To_Simulate(self.robotId)
         
@@ -27,26 +28,31 @@ class ROBOT:
             if self.nn.Is_Motor_Neuron(neuronName):
                 jointName = self.nn.Get_Motor_Neurons_Joint(neuronName)
                 desiredAngle = self.nn.Get_Value_Of(neuronName)
-                #self.motors[jointName].Set_Values(self.robotId, desiredAngle)
-                for i in self.motors: 
-                    for i in self.motors: 
-                        self.motors[i].Set_Values(self.robotId, desiredAngle)
-                print(neuronName, jointName, desiredAngle)
+                self.motors[jointName].Set_Values(self.robotId, desiredAngle)
+##                print(neuronName)
+##                print(jointName)
+##                print(desiredAngle)
+##                for i in self.motors: 
+##                    for i in self.motors: 
+##                        self.motors[i].Set_Values(self.robotId, desiredAngle)
+##                print(neuronName, jointName, desiredAngle)
+
+        self.t += 1 
 
 
     def Prepare_To_Sense(self):
         self.sensors = {}
         for linkName in pyrosim.linkNamesToIndices:
             self.sensors[linkName] = SENSOR(linkName)
+
+        self.t = 0
             
-    def Sense(self, timeStep):
-        self.timeStep = timeStep
-        for i in self.sensors:
-            self.sensor = self.sensors[i]
-            self.sensor.Get_Value(self.timeStep)
+    def Sense(self):
+        for linkName in pyrosim.linkNamesToIndices:
+            self.sensors[linkName].Get_Values(self.t)
 
     def Think(self):
         self.nn.Update()
-        self.nn.Print()
+        #self.nn.Print()
 
 
